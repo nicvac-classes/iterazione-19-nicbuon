0a2ff8f6762fa5ba4fda54281282b251606d74fc
nicbuon <nicola.buonavoglia@marconibari.edu.it>
2024-11-07 09:11:56 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+int main() {
+    int c, d, i, q, r;
+
+    cout << "Inserisci un numero" << endl;
+    cin >> c;
+    i = 0;
+    for (d = 1; d <= c; d++) {
+        q = (int) ((double) c / d);
+        r = c % d;
+        if (r == 0) {
+            cout << d << "è divisore di" << c << endl;
+            i = i + 1;
+        }
+    }
+    if (i == 2) {
+        cout << c << " è un numero primo" << endl;
+    } else {
+        cout << d << " non è un numero primo" << endl;
+    }
+    
+
+
